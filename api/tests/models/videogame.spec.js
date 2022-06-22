d25f38366f1678f03f16c8b317d933e04edbe096
Celeste Cancio <celestecancio@gmail.com>
2022-06-22 18:01:32 -0300

    Trabaja en el test del modelo de Videogame

--- api/tests/models/videogame.spec.js
+++ api/tests/models/videogame.spec.js
@@ -9,13 +9,37 @@ describe('Videogame model', () => {
   describe('Validators', () => {
     beforeEach(() => Videogame.sync({ force: true }));
     describe('name', () => {
-      it('should throw an error if name is null', (done) => {
-        Videogame.create({})
+      // it('should throw an error if name is null', (done) => {
+      //   Videogame.create({})
+      //     .then(() => done(new Error('It requires a valid name')))
+      //     .catch(() => done());
+      // });
+      it('should work when its a valid name, description and platforms', () => {
+        Videogame.create({ 
+          name: 'Cars',
+          description: 'A game about cars',
+          platforms:'I do not know what goes here' });
+      });
+      it('should should not create a videogame if name is not sent', (done) => {
+        Videogame.create({
+          description: 'A game about cars',
+          platforms:'I do not know what goes here'})
           .then(() => done(new Error('It requires a valid name')))
           .catch(() => done());
       });
-      it('should work when its a valid name', () => {
-        Recipe.create({ name: 'Super Mario Bros' });
+      it('should should not create a videogame if description is not sent', (done) => {
+        Videogame.create({
+          name: 'Cars race',
+          platforms:'I do not know what goes here'})
+          .then(() => done(new Error('It requires a valid description')))
+          .catch(() => done());
+      });
+      it('should should not create a videogame if platforms is not sent', (done) => {
+        Videogame.create({
+          name: 'Cars race',
+          description: 'A game about cars'})
+          .then(() => done(new Error('It requires a valid platform')))
+          .catch(() => done());
       });
     });
   });
