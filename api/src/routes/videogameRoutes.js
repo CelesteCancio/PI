c7dbb25375995ee668458e5d945c9096b3974d4e
Celeste Cancio <celestecancio@gmail.com>
2022-06-22 19:19:58 -0300

    Crea esqueleto de rutas de Videogame y Videogames

--- api/src/routes/videogameRoutes.js
+++ api/src/routes/videogameRoutes.js
@@ -1,11 +1,13 @@
 const {Router} = require ("express");
+const {getVideogameById} = require ('../controllers/videogameControllers');
 
 const router = Router();
 
-router.post('/', (req,res) => {
-
+router.get('/:idVideogame', (req,res) => {
+    const {idVideogame} = req.params;
     try {
-        return res.send("Estas en videogame/");
+        const fc = getVideogameById(idVideogame);
+        return res.send(`Estas en un GET de videogame/, mandaste por params el id ${idVideogame} y se acaba de llamar la funcion ${fc}`);
     } catch (error) {
         return res.send(error);
     }
--- api/src/routes/videogamesRoutes.js
+++ api/src/routes/videogamesRoutes.js
@@ -1,12 +1,29 @@
 const {Router} = require ("express");
+const {addVideogame, getVideogames, getVideogamesByName} = require ("../controllers/videogamesControllers");
+
 
 const router = Router();
 
+router.get('/', (req,res) => {
+    const {name} = req.query;
+    try {
+        if(name){            
+            const fc = getVideogamesByName(name);
+            return res.send(`Estas en un GET de videogames/, mandaste por query el nombre ${name} y se acaba de llamar la funcion ${fc}`);
+        }
+        const fc = getVideogames();
+        return res.send(`Estas en un GET de videogames/ y se acaba de llamar la funcion ${fc}`);
+    } catch (error) {
+        return res.send(error);
+    }
+});
 
-router.post('/', (req,res) => {
+router.post('/', async (req,res) => {
 
     try {
-        return res.send("Estas en videogames/");
+        const addedVideogame = await addVideogame({...req.body});
+        //return res.send(`Estas en un POST de videogames/ y se acaba de llamar la funcion ${fc}`);
+        return res.json(addedVideogame);
     } catch (error) {
         return res.send(error);
     }
